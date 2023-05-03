1f1e12e8a5c9f1b8c88f0b7e8806223ccaac8d0e
mehedi9868 <mehedi9868@gmail.com>
2023-05-04 02:46:58 +0600

    hide fire base config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBVKncepoVhLVaEsfO8lg7eOpGJL1krvVM",
-    authDomain: "italia-foodie.firebaseapp.com",
-    projectId: "italia-foodie",
-    storageBucket: "italia-foodie.appspot.com",
-    messagingSenderId: "444475832623",
-    appId: "1:444475832623:web:3469eef56402551cd549da"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
