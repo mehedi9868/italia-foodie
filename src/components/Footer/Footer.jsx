44cdd61545ddcad7d07310c40c67be36e9641074
mehedi9868 <mehedi9868@gmail.com>
2023-05-03 22:55:14 +0600

    added reasonable and meaningful footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,20 +3,18 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
+        <footer className="footer p-10 bg-base-100 text-base-content">
             <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                <span className="footer-title">Explore</span>
+                <a className="link link-hover">Recipes</a>
+                <a className="link link-hover">Ingredients</a>
+                <a className="link link-hover">Cuisines</a>
             </div>
             <div>
-                <span className="footer-title">Company</span>
+                <span className="footer-title">Connect</span>
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <a className="link link-hover">Blog</a>
             </div>
             <div>
                 <span className="footer-title">Legal</span>
@@ -25,7 +23,7 @@ const Footer = () => {
                 <a className="link link-hover">Cookie policy</a>
             </div>
             <div>
-                <span className="footer-title">Newsletter</span>
+                <span className="footer-title">Join our Newsletter</span>
                 <div className="form-control w-80">
                     <label className="label">
                         <span className="label-text">Enter your email address</span>
@@ -35,6 +33,9 @@ const Footer = () => {
                         <button className="btn btn-primary absolute top-0 right-0 rounded-l-none">Subscribe</button>
                     </div>
                 </div>
+            </div>
+            <div className="mt-8 text-sm text-center text-gray-500">
+                &copy; {new Date().getFullYear()} Italia Foodie. All rights reserved.
             </div>
         </footer>
     );
